0e7ce60a7c99817ffcecec84a71aac4f3fc19189
Robs79cv <robsontec.viana79@gmail.com>
2023-09-04 21:36:46 -0300

    Aula 03

---
+++ App.js
@@ -0,0 +1,6 @@
+import Login from './Screens/Login';
+const App = () => {
+  return <Login />;
+};
+
+export default App;
---
+++ Screens/Login.js
@@ -0,0 +1,36 @@
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+
+const Login = () => {
+  return (
+    <View style={estilos.container}>
+      <Text style={estilos.titulo}>Login</Text>
+      <TextInput style={estilos.input} placeholder="Email" keyboardType="email-address" />
+      <TextInput style={estilos.input} placeholder="Senha" secureTexEntry={true} />
+      <Button title="Entrar"/>
+    </View>
+  );
+};
+
+const estilos = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'snow',
+    paddingHorizontal: 16,
+    justifyContent: 'center'
+  },
+  titulo: {
+    fontSize: 26,
+    fontWeight: '400',
+    textAlignt: "center",
+    marginBottom: 24,
+  },
+  input: {
+    lineHeight: 48,
+    borderBottomWidth: 1,
+    borderBottomColor: 'lightgray',
+    marginBottom: 16,
+  },
+ 
+});
+
+export default Login;
